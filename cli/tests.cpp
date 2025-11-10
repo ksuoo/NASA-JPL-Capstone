fc15216b4134203140eaf10dd7079da91175a227
Peyton Barger <peba1207@colorado.edu>
2025-11-10 11:45:54 -0700

    tests fixed

--- cli/tests.cpp
+++ cli/tests.cpp
@@ -13,10 +13,10 @@ TEST_CASE("line is parsed correctly", "[parsing]") {
 	REQUIRE(result[2] == "a");
 	REQUIRE(result[3] == "test");
 
-    string line = "group \"the part in quotes\"";
-    vector<string> result = parseLine(line);
-	REQUIRE(result[0] == "group");
-	REQUIRE(result[1] == "the part in quotes");
+    string line2 = "group \"the part in quotes\"";
+    vector<string> result2 = parseLine(line2);
+	REQUIRE(result2[0] == "group");
+	REQUIRE(result2[1] == "the part in quotes");
 }
 
 TEST_CASE("Accepts and understands prompts", "[VLM]"){
@@ -26,14 +26,14 @@ TEST_CASE("Accepts and understands prompts", "[VLM]"){
     REQUIRE(response.find("hello world") != string::npos);
 
     //test image
-    string response = generate(parseLine("generate \"What shape is this\" testImages/circle.png"));
+    string response2 = generate(parseLine("generate \"What shape is this\" testImages/circle.png"));
     //convert to lowercase
-    transform(response.begin(), response.end(), response.begin(), ::tolower);
-    REQUIRE(response.find("circle") != string::npos);
+    transform(response2.begin(), response2.end(), response2.begin(), ::tolower);
+    REQUIRE(response2.find("circle") != string::npos);
 
     //test two images
-    string response = generate(parseLine("generate \"What are these two shapes\" testImages/circle.png testImages/triangle.png"));
-    transform(response.begin(), response.end(), response.begin(), ::tolower);
-    REQUIRE(response.find("circle") != string::npos);
-    REQUIRE(response.find("triangle") != string::npos);
+    string response3 = generate(parseLine("generate \"What are these two shapes\" testImages/circle.png testImages/triangle.png"));
+    transform(response3.begin(), response3.end(), response3.begin(), ::tolower);
+    REQUIRE(response3.find("circle") != string::npos);
+    REQUIRE(response3.find("triangle") != string::npos);
 }
